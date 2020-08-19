05e9e7fbd2ec857c84bd9967559f265c5b02bfb5
Charly98cma <1998cma@gmail.com>
2020-08-19 11:28:20 +0200

    Added first requests of the new Launch Library API

--- nextflight.py
+++ nextflight.py
@@ -3,7 +3,7 @@ from telegram import Update, ParseMode
 from telegram.ext import Updater, CommandHandler
 
 # LAUNCHLIBRARY
-import launchlibrary as ll 
+import requests
 
 # USEFUL LIBRARIES
 import logging
@@ -16,29 +16,41 @@ import os
 # Useful for debuging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+# Basic URL of the API
+URL = "https://ll.thespacedevs.com/2.0.0"
+
 
 # Processing commands
 def start_Command(update, context):
-    welcome_message = 'Hello there :D\n' + \
-        'I can help you keep track of the next rocket launch.\n\n' + \
-        '**Commands to control me:c**\n' +\
-        '*/help* - Display the list of commands\n' +\
-        '*/next* - Information about next lauch\n' +\
-        '*/cancel* - Ends the conversation'
-    update.message.reply_text(welcome_message, parse_mode=ParseMode.MARKDOWN)
+    welcome_msg = "Hello there!\n" + \
+        "I can help you keep track of the next rocket launch, you just need to ask :D\n\n" + \
+        "<b>Commands to control me:</b>\n" +\
+        "/help - Display the list of commands\n" +\
+        "/next - Information about next lauch\n" +\
+        "/cancel - Ends the conversation"
+    update.message.reply_text(welcome_msg, parse_mode=ParseMode.HTML)
 
     
 def help_Command(update, context):
-    h = "WIP"
+    # Gives the user the list of commands
+    help_msg = "<b>Commands to control me:</b>\n" +\
+        "/help - Display the list of commands\n" +\
+        "/next - Information about next lauch\n" +\
+        "/cancel - Ends the conversation"
+    update.message.reply_text(help_msg, parse_mode=ParseMode.HTML)
 
     
 def nextflight_Command(update, context):
-    h = "WIP"
+    # API request to retrive the next space flight
+    response = requests.get(URL+"/launch", params={"limit" : 1}).json()
+    results = response["results"][0]
+    name = results["name"]
+    
+    #update.message.reply_text(results["name"], parse_mode=ParseMode.HTML)
 
 
 def cancel_Command(update, context):
     h = "WIP"
-
 
 
     
@@ -50,14 +62,13 @@ def main():
     # Dispatcher to register handlers
     dp = updater.dispatcher
     
-    # Initializing Launchlibrary API connection
-    api = ll.Api()
-    
     # Handlers for commands
     dp.add_handler(CommandHandler('start', start_Command))
     dp.add_handler(CommandHandler('help', help_Command))
     dp.add_handler(CommandHandler('nextflight', nextflight_Command))
     dp.add_handler(CommandHandler('cancel', cancel_Command))
+
+    
     
     # Getting starter for Updates
     updater.start_polling(clean = True)
